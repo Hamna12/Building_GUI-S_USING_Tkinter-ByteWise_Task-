b095ac1948639500b37ff004714eba3dd1bdef5c
Hamna Qaseem <72624285+Hamna12@users.noreply.github.com>
2022-10-15 16:12:07 +0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,15 @@
+
+import tkinter
+from tkinter import *
+
+root = Tk()
+
+#creating a label widget
+myLabel = Label(root, text="Hello World!")
+
+# Showing it onto the screen
+myLabel.pack()
+
+root.mainloop()
+
+
